d658c20a76599269051ce5f67a585fda469d6593
Vladimír Ulman <ulman@fi.muni.cz>
2021-12-06 10:14:02 +0100

    ADD: test data & finished the example calls showing how to use the API

--- src/test/java/net/celltrackingchallenge/measures/TestMeasures.java
+++ src/test/java/net/celltrackingchallenge/measures/TestMeasures.java
@@ -19,10 +19,9 @@ public class TestMeasures
 	public void calc_SEG()
 	{
 		try {
-			logService.setLevel(0);
-			//a demo how to silence from any logging,
-			//btw, the reporting below does not make sense with this setLevel
-			//and all could be set to false
+			//uncomment the following to silence the measures completely
+			//...in which case the enabling of the reporting below becomes rather pointless... ;-)
+			//logService.setLevel(0);
 
 			final SEG seg = new SEG(logService);
 			seg.doLogReports = true;          //reports scores associated with every GT label
@@ -33,12 +32,12 @@ public class TestMeasures
 			//or leave unset to make the SEG take all files from the input folder
 			seg.doOnlyTheseTimepoints = new TreeSet<>();
 			seg.doOnlyTheseTimepoints.add(2);
-			//seg.doOnlyTheseTimepoints.add(5);
+			seg.doOnlyTheseTimepoints.add(5);
 
-			double segValue = seg.calculate("/temp/test/gt","/temp/test/res_forSEG");
+			double segValue = seg.calculate(folder_GT, folder_result);
 			System.out.println("Got SEG value = "+segValue);
 		} catch (IOException e) {
-			System.out.println("SEG Error: "+e.getMessage());
+			System.out.println("SEG error: "+e.getMessage());
 			e.printStackTrace();
 		}
 	}
@@ -47,10 +46,9 @@ public class TestMeasures
 	public void calc_TRAandDET()
 	{
 		try {
-			logService.setLevel(0);
-			//a demo how to silence from any logging,
-			//btw, the reporting below does not make sense with this setLevel
-			//and all could be set to false
+			//uncomment the following to silence the measures completely
+			//...in which case the enabling of the reporting below becomes rather pointless... ;-)
+			//logService.setLevel(0);
 
 			final TRA tra = new TRA(logService);
 			tra.doLogReports = true;          //reports scores associated with every GT label and link
@@ -58,24 +56,74 @@ public class TestMeasures
 
 			//TRA always takes all files from the input folder
 
-			double traValue = tra.calculate("/temp/test/gt","/temp/test/res_forTRA");
+			double traValue = tra.calculate(folder_GT, folder_result);
 			System.out.println("Got TRA value = "+traValue);
 
 			final DET det = new DET(logService);
 			//NB: the 3rd param enables to re-use the most from the previous TRA call,
 			//    for example the image data is not loaded again
-			double detValue = det.calculate("/temp/test/gt","/temp/test/res_forTRA", tra.getCache());
+			double detValue = det.calculate(folder_GT, folder_result, tra.getCache());
 			System.out.println("Got DET value = "+detValue);
 		} catch (IOException e) {
-			System.out.println("TRA Error: "+e.getMessage());
+			System.out.println("TRA or DET error: "+e.getMessage());
 			e.printStackTrace();
 		}
 	}
 
 
+	public void calc_everything()
+	{
+		try {
+			//silence...
+			final int prevLogLevel = logService.getLevel();
+			//logService.setLevel(0);
+
+			final SEG seg = new SEG(logService);
+			final TRA tra = new TRA(logService);
+			final DET det = new DET(logService);
+			//
+			final CT  ct  = new CT(logService);
+			final TF  tf  = new TF(logService);
+			final CCA cca = new CCA(logService);
+			final BCi bci = new BCi(logService);
+			bci.setI(2);
+
+			double segValue = seg.calculate(folder_GT, folder_result);
+			double traValue = tra.calculate(folder_GT, folder_result);
+			final TrackDataCache sharedCache = tra.getCache();
+			double detValue = det.calculate(folder_GT, folder_result, sharedCache);
+			double ctValue = ct.calculate(folder_GT, folder_result, sharedCache);
+			double tfValue = tf.calculate(folder_GT, folder_result, sharedCache);
+			//throws exception on the test data: "GT tracking data show no complete cell cycle!"
+			//note: BCi also does not make too much sense on the test data from this repo
+			//double ccaValue = cca.calculate(folder_GT, folder_result, sharedCache);
+			double ccaValue = -1.0;
+			double bciValue = bci.calculate(folder_GT, folder_result, sharedCache);
+
+			//restore previous verbosity
+			logService.setLevel(prevLogLevel);
+
+			System.out.println("SEG: "+segValue
+					+"\nTRA: "+traValue
+					+"\nDET: "+detValue
+					+"\nCT: "+ctValue
+					+"\nTF: "+tfValue
+					+"\nCCA: "+ccaValue
+					+"\nBCi: "+bciValue);
+		} catch (IOException e) {
+			System.out.println("Measures error: "+e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+
+	final String folder_GT = "/temp/test/GT";
+	final String folder_result = "/temp/test/res";
+
 	public static void main(String[] args) {
 		final TestMeasures tst = new TestMeasures();
 		//tst.calc_SEG();
-		tst.calc_TRAandDET();
+		//tst.calc_TRAandDET();
+		tst.calc_everything();
 	}
 }
